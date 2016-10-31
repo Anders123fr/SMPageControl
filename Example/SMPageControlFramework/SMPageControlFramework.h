1575c3db8577f0018e0e1054e80cfcee325fabd2
Anders Friis <af@andersfr.dk>
2016-10-31 10:48:31 +0100

    Carthage support

---
+++ Example/SMPageControlFramework/SMPageControlFramework.h
@@ -0,0 +1,21 @@
+//
+//  SMPageControlFramework.h
+//  SMPageControlFramework
+//
+//  Created by Anders Friis on 31/10/2016.
+//  Copyright © 2016 Spaceman Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SMPageControlFramework.
+FOUNDATION_EXPORT double SMPageControlFrameworkVersionNumber;
+
+//! Project version string for SMPageControlFramework.
+FOUNDATION_EXPORT const unsigned char SMPageControlFrameworkVersionString[];
+
+
+
+#import <SMPageControl/SMPageControl.h>
+
+
